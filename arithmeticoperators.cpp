f7a754f7a3b1d512e40537a83ca3268f6601fa90
gitpher <jun_chois@naver.com>
2022-11-20 13:31:22 +0900

    watermelons.cpp + arithmeticoperators.app

---
+++ arithmeticoperators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+    using namespace std;
+
+    int a = 2;
+    int b = 3;
+
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b << endl;
+    cout << "a * b = " << a * b << endl;
+    cout << "a / b = " << a / b << endl;
+    cout << "a % b = " << a % b << endl;
+
+
+    return 0;
+}
