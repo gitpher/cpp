986515c0741dda3adef4a03bba337960c020ec66
gitpher <jun_chois@naver.com>
2022-11-20 19:05:51 +0900

    incremental.cpp

---
+++ incremental.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+int main() {
+
+
+    using namespace std;
+
+
+    int watermelons = 3;
+
+    cout << "watermelons: " << watermelons << endl;
+
+    cout << "watermelons++: " << watermelons++ << endl;
+    
+    cout << "watermelons: " << watermelons << endl;
+
+    cout << "++watermelons: " << ++watermelons << endl;
+
+    cout << "watermelons--: " << watermelons-- << endl;
+
+    cout << endl;
+
+    cout << "watermelons: " << watermelons << endl;
+
+
+    return 0;
+}
