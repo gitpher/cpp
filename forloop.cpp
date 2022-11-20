0916b6c7498594aeb07b541e82554e131be48bf1
gitpher <jun_chois@naver.com>
2022-11-20 23:01:50 +0900

    forloop.cpp

---
+++ forloop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+    using namespace std;
+
+    for (int i=0; i < 10; i++) {
+
+        cout << "i + 1: " << (i + 1) << endl;
+
+    }
+
+    cout << "end" << endl;
+
+    return 0;
+}
