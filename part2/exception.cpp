5bb70e3de4f99a1e6e93ade1af181319ae98956c
gitpher <jun_chois@naver.com>
2022-11-23 00:12:14 +0900

    exception.cpp

---
+++ part2/exception.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    int num[3] = {11, 22, 33};
+    int input = 0;
+
+    cout << "Plese enter integer between 1~3" << endl;
+    cin >> input;
+
+    try {
+        if (input < 1 || input > 3) {
+            throw input;
+        }
+
+        cout << num[input -1] << endl;
+    } catch (int e) {
+        cout << "INVALID INPUT: " << e << endl;
+    }
+
+    cout << "DONE" << endl;
+
+
+    return 0;
+}
